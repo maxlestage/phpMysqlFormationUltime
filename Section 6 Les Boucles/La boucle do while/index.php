8ec9b6d4bc85662c4598a510ebf0df5e1e390376
Max Lestage <maxlestage@icloud.com>
2020-08-26 10:31:51 +0200

    boucle do while & boucle imbriquée

---
+++ Section 6 Les Boucles/La boucle do while/index.php
@@ -0,0 +1,19 @@
+<?php
+
+// LA BOUCLE DO - WHILE
+// le code ci présent sera traité au moins une seule fois
+// et a la fin seon si ca retombe vrai ou faux, il re traitera le code.    
+$x = 0;
+
+do {
+    
+    echo 'Le nombre est égal : '.$x.'<br/>';
+    // Ici la boucle va rajouter +1 a $x a chaque tour de la boucle. 
+    $x++;
+} while ($x < 10)
+
+
+
+
+?>
+
---
+++ Section 6 Les Boucles/Les boucles imbriqué/index.php
@@ -0,0 +1,37 @@
+<?php
+// Boucles Imbriquées
+
+/*for ($i = 0; $i <= 5; $i++){
+
+    for ($j = 0; $j <= 5; $j++) {
+        echo $i*$j.' '; 
+    }
+    echo '<br/>'; 
+}*/
+
+// Exercice Tableau de Multiplication
+
+echo '<table border style="border-collapse: collapse;">
+    <tr>
+        <th>1</th>
+        <th>2</th>
+        <th>3</th>
+        <th>4</th>
+        <th>5</th>
+        <th>6</th>
+        <th>7</th>
+        <th>8</th>
+        <th>9</th>
+        <th>10</th>
+    </tr>
+    </table>'; 
+
+for($i = 0; $i <= 10; $i++){
+
+    echo '<tr>';
+
+}
+
+
+
+?>
